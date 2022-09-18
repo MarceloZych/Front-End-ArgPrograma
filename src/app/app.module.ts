77fcdc5295f9020033b160ec5a88a9bba5dc02b5
MarceloZych <74663103+MarceloZych@users.noreply.github.com>
2022-09-18 00:06:07 -0300

    Inicio de Portafolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SobreMiComponent,
+    CarouselComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
